66b0cb7ea96f9c409cfdf524ed7393b417678d49
AKKI1903 <ankush.akki19@gmail.com>
2024-09-16 21:08:04 +0200

    Parallel Run

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,15 +2,23 @@ import {devices, PlaywrightTestConfig} from "@playwright/test"
 
 
 const config: PlaywrightTestConfig = {
+  projects:[{
+    name: 'chromium',
+    use: { ...devices['Desktop Chrome'], viewport: { width: 1920, height: 1080 } }
+  },
+  {
+    name: 'firefox',
+    use: { ...devices['Desktop Firefox'], viewport: { width: 1920, height: 1080 } }
+  }],
   use: {
     baseURL:"https://www.lambdatest.com/selenium-playground/",
     headless: false,
     screenshot: "on",
     video: "on",
-    viewport: null,
+ /*    viewport: null,
     launchOptions: {
       args: ['--start-maximized']
-    }
+    } */
   },
   reporter:[["json", {
     outputFile: "report/jsonReport.json"
